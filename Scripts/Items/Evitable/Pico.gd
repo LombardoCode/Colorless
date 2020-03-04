3012dcfe5c2e029942d17e9fb7a67b26e38ee9ef
Lombardo Moreno Rodríguez <LombardoBusiness@gmail.com>
2020-03-04 17:45:29 -0600

    Niveles del 24 al 28 realizados.

--- Scripts/Items/Evitable/Pico.gd
+++ Scripts/Items/Evitable/Pico.gd
@@ -5,34 +5,58 @@ export(bool) var b_mov_v = false
 export(float) var desplazamiento
 export(float) var direccion
 
+# Trampa
+export(bool) var trampa
+var trampa_activada = false
+export(bool) var mov_trampa_h
+export(bool) var mov_trampa_v
+export(int) var trampa_direccion
+export(float) var trampa_velocidad
+export(bool) var trampa_inciar_auto = false
+
+
+
+
 var motion = Vector2()
 var area
 
 func _ready():
+	if self.has_node("Area2D_Trampa"):
+		$Area2D_Trampa.connect("body_entered", self, "_activar_trampa")
 	pass
 
 func _process(delta):
 	$Label.text = str(direccion)
-	if b_mov_h == true:
-		if $Ray_izq.is_colliding() and $Ray_izq.get_collider().is_in_group("tilemap"):
-			direccion = 1
-		#elif $Ray_izq.is_colliding() and $Ray_izq.get_collider().name == "Personaje":
-		#	Global.personaje.muerto = true
-		if $Ray_der.is_colliding() and $Ray_der.get_collider().is_in_group("tilemap"):
-			direccion = -1
-		#elif $Ray_der.is_colliding() and $Ray_der.get_collider().name == "Personaje":
-		#	Global.personaje.muerto = true
-		if direccion == 1:
-			motion = Vector2(desplazamiento, 0)
-		elif direccion == -1:
-			motion = Vector2(-desplazamiento, 0)
-		motion = move_and_slide(motion, Vector2(0, -1))
+	if !trampa:
+		if b_mov_h == true:
+			if $Ray_izq.is_colliding() and $Ray_izq.get_collider().is_in_group("tilemap"):
+				direccion = 1
+			if $Ray_der.is_colliding() and $Ray_der.get_collider().is_in_group("tilemap"):
+				direccion = -1
+			if direccion == 1:
+				motion = Vector2(desplazamiento, 0)
+			elif direccion == -1:
+				motion = Vector2(-desplazamiento, 0)
+			motion = move_and_slide(motion, Vector2(0, -1))
+	else:
+		pass
 		
-		for i in range(get_slide_count()):
-			var collision = get_slide_collision(i)
-			var nombre_colisionador = collision.collider.name
-			if nombre_colisionador == "Personaje":
-				Global.personaje.muerto = true
-		
-		
-
+	for i in range(get_slide_count()):
+		var collision = get_slide_collision(i)
+		var nombre_colisionador = collision.collider.name
+		if nombre_colisionador == "Personaje":
+			Global.personaje.muerto = true
+	
+	if trampa_activada or trampa_inciar_auto:
+		if trampa:
+			if mov_trampa_h:
+				motion = move_and_slide(Vector2(trampa_velocidad * trampa_direccion, 0), Vector2(0, -1))
+			elif mov_trampa_v:
+				motion = move_and_slide(Vector2(0, trampa_velocidad * trampa_direccion), Vector2(0, -1))
+	
+	
+func _activar_trampa(body):
+	if body.get_name() == "Personaje":
+		trampa_activada = true
+	
+	
